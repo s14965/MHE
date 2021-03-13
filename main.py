6c9870c6b4c10b58c0cdccdf28177e93db0269f0
s14965 <s14965@pjwstk.edu.pl>
2021-03-13 20:52:38 +0100

    Graph generation and random solution

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import inputhandler
 import utility
 import bruteforce
 
-nodes, edges = inputhandler.handlingInput(sys.argv)
+input_nodes, input_edges = inputhandler.handlingInput(sys.argv)
 
-def goalFunction(solution, edges):
+def goalFunction(solution, nodes, edges):
     evaluations = []
     for s in solution:
         evaluations.append(utility.countConnections(s, edges))
@@ -19,11 +19,21 @@ def goalFunction(solution, edges):
                                          * ( len(nodes) - len(solution) ) 
                                          * ( len(solution)  - 1 ) )
 
-print(goalFunction([6,7,4,5], edges))
-print(goalFunction([2,3,4,6], edges))
-print(goalFunction([2,3,4,7], edges))
-print(goalFunction([2,3,4,5], edges))
-
-solution = bruteforce.bruteforce(nodes, edges)
-print(goalFunction(solution, edges))
+print("from input")
+print(goalFunction([6,7,4,5], input_nodes, input_edges))
+print(goalFunction([2,3,4,6], input_nodes, input_edges))
+print(goalFunction([2,3,4,7], input_nodes, input_edges))
+print(goalFunction([2,3,4,5], input_nodes, input_edges))
+solution = bruteforce.bruteforce(input_nodes, input_edges)
+print(goalFunction(solution, input_nodes, input_edges))
 print(solution)
+
+print()
+print("from rand")
+nodes2, edges2 = utility.generateGraph(graphSize = 20)
+rand_solution = utility.randSolution(nodes2)
+print(rand_solution)
+print(goalFunction(rand_solution, nodes2, edges2))
+brut_solution = bruteforce.bruteforce(nodes2, edges2)
+print(goalFunction(brut_solution, nodes2, edges2))
+print(brut_solution)
--- utility.py
+++ utility.py
@@ -18,7 +18,7 @@ def isclique(solution, edges):
     return True
 
 def randSolution(nodes):
-    size = random.randrange(3, len(nodes))
+    size = random.randint(3, len(nodes))
     
     solution = []
     i=0
@@ -28,4 +28,35 @@ def randSolution(nodes):
             solution.append(choice)
             i += 1
     return solution
-        
+
+def shuffle(nodes):
+    tmp = nodes.copy()
+    tmp2 = []
+    for i in range(len(nodes)):
+        rand = random.choice(tmp)
+        tmp2.append(rand)
+        tmp.pop(tmp.index(rand))
+    return tmp2
+
+def generateGraph(graphSize = 20, edgeRate = 0.7):
+    nodes=[]
+    edges=[]
+    i=0
+    while i < graphSize:
+        i+=1
+        nodes.append(i)
+    nodes = shuffle(nodes)
+    for node in nodes:
+        tmp = []
+        edgeCount = random.randint(2, graphSize * edgeRate)                           # losowanie ilosci krawedzi dla wezla
+        for i in range(0, edgeCount):
+            if len(set(nodes) - set(tmp)) > 0:
+                tmp.append(random.choice(list(set(nodes)-set(tmp)-set([node]))))            # jesli zadna krawedz nie laczy sie z clique'a, to wylosowac jeden z wezlow clique'i dla ostatniej krawedzi
+        for n in tmp:
+            edge = [node, n]
+            edge.sort()
+            if edge not in edges:
+                edges.append(edge)
+                edges.sort()
+        del tmp    
+    return [nodes, edges]
